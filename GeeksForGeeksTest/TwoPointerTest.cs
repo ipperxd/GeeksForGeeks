644082dc58d87678ce56941b22897773cbe1b5fc
ipperxd <ipperxd@gmail.com>
2025-12-23 10:32:55 +0100

    Getting startet with two pointer

---
+++ GeeksForGeeksTest/TwoPointerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoPointers;
+
+namespace GeeksForGeeksTest;
+public class TwoPointerTest
+{
+   TwoPointerImplementation pointerImplementation = new TwoPointerImplementation();
+
+    [Fact]
+    public void TestAlgorithm_PairExists_ReturnsTrue()
+    {
+        int[] input = { 1, 2, 3, 4, 5 };
+        int target = 9;
+        bool result = pointerImplementation.Algorithm(input, target);
+        Assert.True(result);
+    }
+    [Fact]
+    public void TestAlgorithm_PairDoesNotExist_ReturnsFalse()
+    {
+        int[] input = { 1, 2, 3, 4, 5 };
+        int target = 10;
+        bool result = pointerImplementation.Algorithm(input, target);
+        Assert.False(result);
+    }
+}
---
+++ TwoPointers/TwoPointerImplementation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoPointers;
+public class TwoPointerImplementation
+{
+    public bool Algorithm(int[] input, int target)
+    {
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right)
+        {
+            // Example operation: print the pair of elements
+            Console.WriteLine($"Left: {input[left]}, Right: {input[right]}");
+            // Move pointers based on some condition
+            while (left < right)
+            {
+                int sum = input[left] + input[right];
+
+                // Check if the sum matches the target
+                if (sum == target)
+                    return true;
+                else if (sum < target)
+                    left++; // Move left pointer to the right
+                else
+                    right--; // Move right pointer to the left
+            }
+            // If no pair is found
+            return false;
+        }
+
+        return false;
+    }
+}
